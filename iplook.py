bf94ad00c71a64ae1069e27f5eae078f855d1c6e
CurvixSsH <127477293+CurvixSsH@users.noreply.github.com>
2023-06-24 16:41:33 -0600

    iplook.py

--- iplook.py
+++ iplook.py
@@ -1,22 +1,19 @@
-import discord
+import discord 
+from discord import Embed
 from discord.ext import commands
 import requests
+from ipaddress import ip_address
+from config import API_KEY, BOT_TOKEN
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-API_KEY = 'APY_KEY' # Reemplaza con tu clave API de (ipgeolocation.io) https://ipgeolocation.io
+bot.remove_command("help")
 
-# Aquí definimos la función para guardar las IP con un nombre en el archivo host.txt
-""" La función recibe 2 argumentos: el nombre y la dirección IP,
-    usamos el modo append (a) para que no borre los datos anteriores. (sin este modo hay errores) """
 def save_ip(name, address):
     with open('host.txt', 'a') as f:
         f.write(f'{name}:{address}\n')
 
-# Esta es la función para buscar una IP por su nombre en el archivo host.txt
-""" Esta función busca línea por línea hasta encontrar una coincidencia y devuelve el nombre y la IP,
-    si no encuentra el nombre devolverá None """
 def search_ip(name):
     with open('host.txt', 'r') as f:
         for line in f:
@@ -24,64 +21,88 @@ def search_ip(name):
                 return line.strip().split(':')[1]
     return None
 
-# Comando para buscar información de una IP a través de la API ipgeolocation 
-""" Esto lo que hace es extraer la información relevante de la respuesta JSON
-utilizando las claves correspondientes en el diccionario json_data
-La información extraída incluye el nombre del país, la ciudad, el proveedor de servicios de Internet (ISP), la latitud y longitud de la ubicación. 
-También se genera un enlace de comprobación de host utilizando la dirección IP especificada """
-
 @bot.command(name="ip")
 async def ip_command(ctx, address):
     print(f"Comando recibido: {ctx.message.content}")
     api_url = f'https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={address}'
-                
-
     response = requests.get(api_url)
-
-
     json_data = response.json()
-
     country = json_data['country_name']
+    country_flag = json_data['country_flag']
     city = json_data['city']
     isp = json_data['isp']
     lat = json_data['latitude']
     lon = json_data['longitude']
     link = "https://check-host.net/ip-info?host=" + address
-
-    # Verificación si existe un guardado del nombre de la IP en el archivo .txt
-    """ Lo que hace es primero abrir el archivo .txt
-        y verificar si está almacenado el nombre de la IP. Si no se encuentra una coincidencia para la dirección IP buscada,
-        se muestra la información detallada sin el nombre asociado."""
+    gif_url = "https://cdn.discordapp.com/attachments/983911867401527316/1059354893690880052/tenor.gif"
     with open('host.txt', 'r') as f:
         hosts = f.readlines()
         for host in hosts:
             parts = host.strip().split(':')
             if len(parts) > 1 and parts[1] == address:
                 name = parts[0]
-                await ctx.send(f"```Información de la dirección IP {address} ({name}): \nPaís: {country} \nCiudad: {city} \nISP: {isp} \nLatitud: {lat} \nLongitud: {lon}``` Enlace: {link} \nhttps://cdn.discordapp.com/attachments/983911867401527316/1059354893690880052/tenor.gif")
+                embed = Embed(title=f"Información de la dirección IP {address} ({name})", color=0x00ff00)
+                embed.set_thumbnail(url=country_flag)
+                embed.add_field(name="País", value=country, inline=False)
+                embed.add_field(name="Ciudad", value=city, inline=False)
+                embed.add_field(name="ISP", value=isp, inline=False)
+                embed.add_field(name="Latitud", value=lat, inline=False)
+                embed.add_field(name="Longitud", value=lon, inline=False)
+                embed.add_field(name="Enlace", value=link, inline=False)
+                embed.set_image(url=gif_url)
+                await ctx.send(embed=embed)
                 return
+    embed = Embed(title=f"Información de la dirección IP {address}", color=0x00ff00)
+    embed.set_thumbnail(url=country_flag)
+    embed.add_field(name="País", value=country, inline=False)
+    embed.add_field(name="Ciudad", value=city, inline=False)
+    embed.add_field(name="ISP", value=isp, inline=False)
+    embed.add_field(name="Latitud", value=lat, inline=False)
+    embed.add_field(name="Longitud", value=lon, inline=False)
+    embed.add_field(name="Enlace", value=link, inline=False)
+    embed.set_image(url=gif_url)
+    await ctx.send(embed=embed)
 
-  
-    await ctx.send(f"```Información de la dirección IP {address}: \nPaís: {country} \nCiudad: {city} \nISP: {isp} \nLatitud: {lat} \nLongitud: {lon}``` Enlace: {link} \nhttps://cdn.discordapp.com/attachments/983911867401527316/1059354893690880052/tenor.gif")
-
-# Comando para guardar IP
-@bot.command(name="host")
-async def host_command(ctx, name, address):
+@bot.command(name="label")
+async def label_command(ctx, name, address):
     print(f"Comando recibido: {ctx.message.content}")
+    try:
+        ip_address(address)
+    except ValueError:
+        embed = Embed(title="La dirección IP no es válida.", color=0xff0000)
+        await ctx.send(embed=embed)
+        return
+    if len(name) > 20:
+        embed = Embed(title="El nombre no puede tener más de 20 caracteres.", color=0xff0000)
+        await ctx.send(embed=embed)
+        return
     save_ip(name, address)
-    await ctx.send(f"La dirección IP {address} ha sido guardada con el nombre {name}.")
+    embed = Embed(title=f"La dirección IP {address} ha sido guardada con el nombre {name}.", color=0xFFFF00)
+    await ctx.send(embed=embed)
 
-# Comando para buscar una  IP por su nombre
 @bot.command(name="search")
 async def search_command(ctx, name):
     print(f"Comando recibido: {ctx.message.content}")
     address = search_ip(name)
     if address:
-        await ctx.send(f"La dirección IP para {name} es: {address}")
+        link = f"https://check-host.net/ip-info?host={address}"
+        embed = Embed(title=f"Resultado de la búsqueda", color=0x0000FF)
+        embed.add_field(name="Nombre", value=name, inline=True)
+        embed.add_field(name="Dirección IP", value=address, inline=True)
+        embed.add_field(name="Enlace", value=link, inline=False)
+        await ctx.send(embed=embed)
     else:
-        await ctx.send(f"No se encontró ninguna dirección IP con el nombre {name}.")
+        embed = Embed(title=f"No se encontró ninguna dirección IP con el nombre {name}.", color=0xff0000)
+        await ctx.send(embed=embed)
 
-bot.run('BOT_TOKEN') # Inserta tu token de bot de Discord 
- # https://discord.com/developers/applications
+@bot.command(name="help")
+async def help_command(ctx):
+    print(f"Comando recibido: {ctx.message.content}")
+    embed = Embed(title="Comandos disponibles", color=0xFFA500) 
+    embed.add_field(name="/ip [dirección IP]", value="Muestra información sobre la dirección IP especificada.", inline=False)
+    embed.add_field(name="/label [nombre] [dirección IP]", value="Guarda la dirección IP con el nombre especificado.", inline=False)
+    embed.add_field(name="/search [nombre]", value="Busca una dirección IP por nombre y muestra la dirección IP encontrada.", inline=False)
+    embed.add_field(name="/help", value="Muestra esta lista de comandos disponibles.", inline=False)
+    await ctx.send(embed=embed)
 
- 
+bot.run(BOT_TOKEN)
