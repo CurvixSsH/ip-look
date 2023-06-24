641a7c2b425f7715bf6fab335a2b2111f79794f9
CurvixSsH <127477293+CurvixSsH@users.noreply.github.com>
2023-06-24 04:51:24 -0600

    iplook.py

--- iplook.py
+++ iplook.py
@@ -8,27 +8,27 @@ intents.presences = False
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-API_KEY = 'API_KEY' # Remplaza con tu clave api de (ipgeolocation.io) https://ipgeolocation.io
+API_KEY = 'APY-KEY' # Reemplaza con tu clave API de (ipgeolocation.io) https://ipgeolocation.io
 
-# Aca definimos la función para guardar las IP con un nombre en el archivo host.txt (debes especificar la ruta correcta del archivo)
-""" la funcion recive 2 argumentos el nombre y la direccion ip,
+# Aquí definimos la función para guardar las IP con un nombre en el archivo host.txt
+""" La función recibe 2 argumentos: el nombre y la dirección IP,
     usamos el modo append (a) para que no borre los datos anteriores. (sin este modo hay errores) """
 def save_ip(name, address):
-    with open('C:\\INSERTE_RUTA\\host.txt', 'a') as f:
+    with open('host.txt', 'a') as f:
         f.write(f'{name}:{address}\n')
 
-# esta es la función para buscar una IP por su nombre en el archivo host.txt (debes especificar la misma ruta que antes)
-""" esta funcion busca linea por linea hasta encontrar una coincidencia y devuelve el nombre y la ip,
-    si no encuentra el nombre devolvera none """
+# Esta es la función para buscar una IP por su nombre en el archivo host.txt
+""" Esta función busca línea por línea hasta encontrar una coincidencia y devuelve el nombre y la IP,
+    si no encuentra el nombre devolverá None """
 def search_ip(name):
-    with open('C:\\INSERTE_RUTA\\host.txt', 'r') as f:
+    with open('host.txt', 'r') as f:
         for line in f:
             if name in line:
                 return line.strip().split(':')[1]
     return None
 
-# Comando para buscar información de una IP atraves de la api ipgeolocation 
-""" esto lo que hace es xtrae la información relevante de la respuesta JSON
+# Comando para buscar información de una IP a través de la API ipgeolocation 
+""" Esto lo que hace es extraer la información relevante de la respuesta JSON
 utilizando las claves correspondientes en el diccionario json_data
 La información extraída incluye el nombre del país, la ciudad, el proveedor de servicios de Internet (ISP), la latitud y longitud de la ubicación. 
 También se genera un enlace de comprobación de host utilizando la dirección IP especificada """
@@ -49,15 +49,15 @@ async def ip_command(ctx, address):
     lon = json_data['longitude']
     link = "https://check-host.net/ip-info?host=" + address
 
-    #verificacion si exixte un guardado del nombre de la ip en el archivo .txt (insertar ruta)
-    """ Lo que hace es primero abrir el archivo.text
-        y verificar si esta almacenada el nombre de la ip Si no se encuentra una coincidencia para la dirección IP buscada,
+    # Verificación si existe un guardado del nombre de la IP en el archivo .txt
+    """ Lo que hace es primero abrir el archivo .txt
+        y verificar si está almacenado el nombre de la IP. Si no se encuentra una coincidencia para la dirección IP buscada,
         se muestra la información detallada sin el nombre asociado."""
-    with open('C:\\INSERTE_RUTA\\host.txt', 'r') as f:
+    with open('host.txt', 'r') as f:
         hosts = f.readlines()
         for host in hosts:
             parts = host.strip().split(':')
-            if parts[1] == address:
+            if len(parts) > 1 and parts[1] == address:
                 name = parts[0]
                 await ctx.send(f"```Información de la dirección IP {address} ({name}): \nPaís: {country} \nCiudad: {city} \nISP: {isp} \nLatitud: {lat} \nLongitud: {lon}``` Enlace: {link} \nhttps://cdn.discordapp.com/attachments/983911867401527316/1059354893690880052/tenor.gif")
                 return
@@ -65,7 +65,7 @@ async def ip_command(ctx, address):
   
     await ctx.send(f"```Información de la dirección IP {address}: \nPaís: {country} \nCiudad: {city} \nISP: {isp} \nLatitud: {lat} \nLongitud: {lon}``` Enlace: {link} \nhttps://cdn.discordapp.com/attachments/983911867401527316/1059354893690880052/tenor.gif")
 
-#comando para guardar ip
+# Comando para guardar IP
 @bot.command(name="host")
 async def host_command(ctx, name, address):
     save_ip(name, address)
@@ -80,5 +80,7 @@ async def search_command(ctx, name):
     else:
         await ctx.send(f"No se encontró ninguna dirección IP con el nombre {name}.")
 
-bot.run('TOKEN_BOT') #inserta tu token de bot de discord 
- #https://discord.com/developers/applications
+bot.run('BOT-TOKEN') # Inserta tu token de bot de Discord EJEMPLO: bot.run('847hd38y4hd38hy834hd83h4d83h')
+ # https://discord.com/developers/applications
+
+ 
